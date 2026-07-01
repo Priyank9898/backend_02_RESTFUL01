05cc563ed742908b4d2b9efa00f7aa3d73280191
Priyank Patel <priyank7422@gmail.com>
2026-07-01 13:05:16 +0530

    feat: add authentication and authorization middleware

--- src/modules/auth/auth.route.js
+++ src/modules/auth/auth.route.js
@@ -2,11 +2,11 @@ import { Router } from "express";
 import validate from "../../common/middleware/validate.middleware.js";
 import RegisterDto from "./dto/register.dto.js";
 import * as controller from "./auth.controller.js";
-import loginDto from "./dto/login.dto.js";
+import LoginDto from "./dto/login.dto.js";
 
 const router = Router();
 
 router.post("/register", validate(RegisterDto), controller.register);
-router.post("/login", validate(loginDto), controller.login);
+router.post("/login", validate(LoginDto), controller.login);
 router.post("/refresh-token");
 router.post("/logout");
--- src/modules/auth/dto/login.dto.js
+++ src/modules/auth/dto/login.dto.js
@@ -1,19 +1,15 @@
 import Joi from "joi";
 import BaseDto from "../../../common/dto/base.dto";
 
-class loginDto extends BaseDto {
+class LoginDto extends BaseDto {
   static schema = Joi.object({
     email: Joi.string().trim().lowercase().email().required(),
     password: Joi.string()
       .min(8)
-      .max(20)
-      .pattern(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).+$/)
-      .required()
-      .messages({
-        "string.pattern.base":
-          "Password must contain at least one uppercase letter, one lowercase letter, and one number",
-      }),
+      .max(64)
+      .pattern(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&]).+$/)
+      .required(),
   });
 }
 
-export default loginDto;
+export default LoginDto;
