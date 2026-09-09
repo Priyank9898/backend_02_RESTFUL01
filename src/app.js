a0c0a1da9548f2232e7ef30f96c763305decc229
Priyank9898 <priyank7422@gmail.com>
2026-09-09 19:49:53 +0000

    feat :  fixed few bugs

--- src/app.js
+++ src/app.js
@@ -9,4 +9,13 @@ app.use(cookieParser());
 
 app.use("/api/auth", authRoute);
 
+app.use((err, req, res, next) => {
+  if (err.isOperational) {
+    res.status(err.statusCode).json({ success: false, message: err.message });
+  } else {
+    console.error(err);
+    res.status(500).json({ success: false, message: "Something went wrong" });
+  }
+});
+
 export default app;
